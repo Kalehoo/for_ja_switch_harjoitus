6c861de9adcb18b5f31a904c64b822fbafac75e3
Lauri Liukkonen <128515937+Kalehoo@users.noreply.github.com>
2023-04-08 14:59:19 +0300

    Initial commit

---
+++ Switch_Harjoitus/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Harjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double initialvalue = 12.00;
+
+            for (int i = 1; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        initialvalue = initialvalue + i;
+                        Console.WriteLine(initialvalue);
+                        break;
+                    case 2:
+                        initialvalue = initialvalue * i;
+                        Console.WriteLine(initialvalue);
+                        break;
+                    case 3:
+                        initialvalue = initialvalue / i;
+                        Console.WriteLine(initialvalue);
+                        break;
+                    case 4:
+                        initialvalue = initialvalue - i;
+                        Console.WriteLine(initialvalue);
+                        break;
+
+
+
+
+                }
+
+
+
+            }
+
+                Console.WriteLine($"\nLopullinen initialvalue on: {initialvalue}");
+                Console.ReadKey();
+
+        }
+    }
+}
